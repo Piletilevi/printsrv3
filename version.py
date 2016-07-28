3cd394d451446b0e6d556b17a6ca771110fd0ded
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-07-28 16:05:08 +0300

    read version from package.json

--- version.py
+++ version.py
@@ -1,1 +1,11 @@
-VERSION = '0.1.91'
+from json import load as loadJSON
+from os import path
+from sys import argv
+
+basedir = path.realpath(path.dirname(argv[0]))
+package_file_name = path.join(basedir, 'package.json')
+
+with open(package_file_name, 'r') as package_json_file:
+    package_json = loadJSON(package_json_file)
+
+VERSION = package_json['version']
