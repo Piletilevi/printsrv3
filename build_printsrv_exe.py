fe16999dcb148ab34947c34ec1138b34428f397f
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-08-04 19:13:12 +0300

    code style

--- build_printsrv_exe.py
+++ build_printsrv_exe.py
@@ -1,16 +1,16 @@
 # coding: utf-8
 
 from distutils.core import setup
-import py2exe
+# import py2exe
 
-console = [
+CONSOLE = [
     {
         "script": "printsrv_exe.py",
         "icon_resources": [(0, "favicon.ico")],
         "dest_base" : "printsrv"
     }]
 
-setup(console = console)
+setup(console = CONSOLE)
 
 # run
 # build_printsrv_exe.py py2exe
