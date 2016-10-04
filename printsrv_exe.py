e31e5d6e4b1973d20a5af101b7dbc48552ecbfba
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-10-04 09:50:47 +0300

    update documentation of printsrv.exe

--- printsrv_exe.py
+++ printsrv_exe.py
@@ -1,21 +1,35 @@
 # coding: utf-8
-""" Main executable for printing tickets and/or fiscal receipts
-    Also used for invoking update procedures
-    Makes sure correct script gets invoked based on provided .plp file
-    If .plp is NOT JSON, then it gets forvarded to printsrv.py;
+""" Main executable for printing tickets and/or fiscal receipts;
+    Also used for invoking update procedures;
+    Defines data and printer types for incoming plp files
+        Data is one of ['tickets', 'fiscal', 'update'];
+        Printer type is either 'tickets' or 'fiscal'.
+    Makes sure correct script gets invoked based on provided .plp file.
+
+    If .plp is NOT JSON, then we take a note of it and convert the file to JSON
+        - PLP_DATA_TYPE = 'tickets'
+
     If .plp is JSON;
-       and info == 'fiscal',
-       and operation in ('sale', 'startshift', 'endshift')
-       then RasoASM gets invoked;
-    If .plp is JSON;
-       and info == 'update',
-       and version = 'GitHub tag' is provided
-       then update script gets invoked;
-    If .plp is JSON;
-       and info == 'fiscal',
-       and operation 'endshift'
-       and version = 'GitHub tag' is provided
-       then update script gets invoked after RasoASM returns.
+        - PLP_DATA_TYPE gets read from 'info' ('tickets' | 'fiscal' | 'update')
+
+    if PLP_DATA_TYPE == 'update'
+        Execute update script and exit.
+
+    if printingDriverVersion provided in PLP doesnot match with version in
+    printsrv/package.json
+        Create the update script that will resume printsrv.exe with current parameters
+        Execute update script and exit.
+
+    If printerType is set in plp data, then
+        - PLP_PRINTER_TYPE is set
+    else
+        - PLP_PRINTER_TYPE gets the value of PLP_DATA_TYPE
+
+    if PLP_PRINTER_TYPE == 'tickets', then
+        printsrv/print_ticket.py gets executed
+    else if PLP_PRINTER_TYPE == 'fiscal', then
+        RasoASM/print_tickets.ipy or
+        RasoASM/print_fiscal_LANG.ipy or gets executed
 """
 from os import environ, path, chdir, execl
 from io import open as ioOpen
@@ -24,7 +38,7 @@ from subprocess import call
 from json import load as loadJSON, dumps as dumpsJSON
 from re import match
 
-# from json import load as loadJSON, dumps as dumpsJSON, dump as dumpJSON
+# To convert "old-good-flat" style plp to JSON data structure
 from plp2json import read_plp_file
 
 BASEDIR = path.realpath(path.dirname(argv[0]))
@@ -70,7 +84,17 @@ try:
         PLP_JSON_DATA = loadJSON(plp_data_file)
 except ValueError:
     PLP_JSON_DATA = read_plp_file(PLP_FILENAME)
+    # Backward compatible
+    PLP_JSON_DATA['info'] = 'tickets'
     PLP_JSON_FILENAME = '{0}.json'.format(PLP_FILENAME)
+else:
+    pass
+
+
+PLP_CONTENT_TYPE = PLP_JSON_DATA['info']
+PLP_PRINTER_TYPE = (PLP_JSON_DATA['printerType'] if 'printerType' in PLP_JSON_DATA else PLP_CONTENT_TYPE)
+
+if PLP_PRINTER_TYPE == 'fiscal':
     with ioOpen(PLP_JSON_FILENAME, 'w', encoding='utf-8') as outfile:
         outfile.write(unicode(dumpsJSON(
             PLP_JSON_DATA,
@@ -79,23 +103,7 @@ except ValueError:
             separators=(',', ': '),
             sort_keys=True
         )))
-    # print dumpsJSON(PLP_JSON_DATA, indent=4, ensure_ascii=False, separators=(',', ': '))
     environ['plp_filename'] = PLP_JSON_FILENAME
-    PLP_FILE_TYPE = 'flat'
-else:
-    PLP_FILE_TYPE = 'json'
-
-if 'info' not in PLP_JSON_DATA: # Backward compatible
-    PLP_JSON_DATA['info'] = 'tickets'
-
-PLP_CONTENT_TYPE = PLP_JSON_DATA['info']
-
-if 'ticketPrinterType' in PLP_JSON_DATA:
-    PLP_PRINTER_TYPE = PLP_JSON_DATA['ticketPrinterType']
-elif 'fiscalPrinterType' in PLP_JSON_DATA:
-    PLP_PRINTER_TYPE = PLP_JSON_DATA['fiscalPrinterType']
-else:
-    PLP_PRINTER_TYPE = PLP_CONTENT_TYPE
 
 
 def call_update(plp_update_to_version):
@@ -127,8 +135,12 @@ elif PLP_PRINTER_TYPE == 'fiscal':
         pass
     except Exception as err:
         print 'Can not print fiscal', RASO_FILENAME, 'Error:', err.code
-    if 'version' in PLP_JSON_DATA:
-        call_update(PLP_JSON_DATA['version'])
+
+if 'printingDriverVersion' in PLP_JSON_DATA:
+    with open(path.join(BASEDIR, 'printsrv', 'package.json'), 'rU') as package_json_file:
+        PACKAGE_JSON_DATA = loadJSON(package_json_file)
+        if PACKAGE_JSON_DATA['version'] != PLP_JSON_DATA['printingDriverVersion']:
+            call_update(PLP_JSON_DATA['printingDriverVersion'])
 
 if PLP_CONTENT_TYPE == 'update':
     call_update(PLP_JSON_DATA['version'])
