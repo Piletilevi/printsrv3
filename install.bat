e2b793ed707b4329c0dc552f25360451e43becca
notcool6 <jaan@piletilevi.ee>
2016-10-10 09:18:09 +0300

    confirm overwrite persistent.ini

--- install.bat
+++ install.bat
@@ -3,9 +3,30 @@ pip install fontname
 pip install qrcode
 pip install wmi
 
-fonts/code128.ttf
-fonts/datamatrix.ttf
-fonts/ean13.ttf
-fonts/pdf417.ttf
+@echo off
+
+fonts\code128.ttf
+fonts\datamatrix.ttf
+fonts\ean13.ttf
+fonts\pdf417.ttf
+
+
+Call :YesNoBox "Copy persistent.ini?"
+if "%YesNo%"=="6" (
+copy /y persistent.ini ..
+)
+
+REM Call :YesNoBox "Scooby happy?"
 
 del install.bat
+exit /b
+
+
+:YesNoBox
+REM returns 6 = Yes, 7 = No. Type=4 = Yes/No
+set YesNo=
+set MsgType=4
+set heading=%~2
+set message=%~1
+echo wscript.echo msgbox(WScript.Arguments(0),%MsgType%,WScript.Arguments(1)) >"%temp%\input.vbs"
+for /f "tokens=* delims=" %%a in ('cscript //nologo "%temp%\input.vbs" "%message%" "%heading%"') do set YesNo=%%a
