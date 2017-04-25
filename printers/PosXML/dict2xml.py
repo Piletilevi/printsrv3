442bc9b05f49a247682809e49c23294c2ce23118
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2017-04-25 18:11:46 +0300

    dict2xml

---
+++ printers/PosXML/dict2xml.py
@@ -0,0 +1,31 @@
+from xml.etree import ElementTree
+
+
+def _ConvertDictToXmlRecurse(parent, dictitem):
+    assert type(dictitem) is not type([])
+
+    if isinstance(dictitem, dict):
+        for (tag, child) in dictitem.iteritems():
+            if str(tag) == '_text':
+                parent.text = str(child)
+            elif type(child) is type([]):
+                for listchild in child:
+                    elem = ElementTree.Element(tag)
+                    parent.append(elem)
+                    _ConvertDictToXmlRecurse(elem, listchild)
+            else:
+                elem = ElementTree.Element(tag)
+                parent.append(elem)
+                _ConvertDictToXmlRecurse(elem, child)
+    else:
+        parent.text = str(dictitem)
+
+def convert(xmldict):
+    """
+    Converts a dictionary to an XML ElementTree Element
+    """
+
+    roottag = xmldict.keys()[0]
+    root = ElementTree.Element(roottag)
+    _ConvertDictToXmlRecurse(root, xmldict[roottag])
+    return root
