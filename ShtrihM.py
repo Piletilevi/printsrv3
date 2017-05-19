4270deed64582b67a477ac67c56b47154c0226c8
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2017-05-19 11:17:03 +0300

    correct field for reverse transaction id

--- ShtrihM.py
+++ ShtrihM.py
@@ -298,10 +298,12 @@ class ShtrihM:
             posxmlPort = self.PLP_JSON_DATA['fiscalData']['cardPaymentUnitSettings']['cardPaymentUnitPort']
             with PosXML(self.feedback, self.bye, {'url': 'http://{0}:{1}'.format(posxmlIP,posxmlPort)}) as posxml:
                 posxml.post('CancelAllOperationsRequest', '')
+                _transactionRequest = 'TransactionRequest' if (self.PLP_JSON_DATA['fiscalData']['operation'] == 'sale') else 'RefundTransactionRequest'
+                _transactionIdField = 'businessTransactionId' if (self.PLP_JSON_DATA['fiscalData']['operation'] == 'sale') else 'saleTransactionId'
                 response = posxml.post(
-                    'TransactionRequest' if (self.PLP_JSON_DATA['fiscalData']['operation'] == 'sale') else 'RefundTransactionRequest',
+                    _transactionRequest,
                     {
-                        'TransactionID': self.PLP_JSON_DATA['fiscalData']['businessTransactionId'],
+                        'TransactionID': self.PLP_JSON_DATA['fiscalData'][_transactionIdField],
                         'Amount'       : card_payment_amount * 100,
                         'CurrencyName' : 'EUR',
                         'PrintReceipt' : 2,
