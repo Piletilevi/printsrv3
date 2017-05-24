b194e73a11dce5dc226ee550cbc3530d4a007f8e
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2017-05-24 13:54:58 +0300

    logging for posxml

--- PosXML.py
+++ PosXML.py
@@ -1,15 +1,17 @@
 # This Python file uses the following encoding: utf-8
 
 # PosXML module
-import                                    requests
-import                                    xmltodict
-import                                    sys
-from os           import path          as path
-from json         import dumps         as dumpsJSON
-from sys          import                  stdin
-from yaml         import load          as loadYAML
-from time         import                  sleep
-from os           import                  chdir
+import                                  requests
+import                                  xmltodict
+import                                  sys
+from datetime   import                  datetime
+from os         import path          as path
+from json       import dumps         as dumpsJSON
+from sys        import                  stdin
+from yaml       import load          as loadYAML
+from yaml       import dump          as dumpYAML
+from time       import                  sleep
+from os         import                  chdir
 
 class PosXML:
     def __init__(self, feedback, bye, options):
@@ -18,11 +20,12 @@ class PosXML:
         self.OPTIONS  = { 'headers': { 'content-type': "application/xml" } }
         for key, val in options.items():
             self.OPTIONS[key] = val
-        self.BASEDIR = path.dirname(sys.executable) if hasattr(sys, "frozen") else path.dirname(__file__)
+        self.BASEDIR  = path.dirname(sys.executable) if hasattr(sys, "frozen") else path.dirname(__file__)
         # chdir(self.BASEDIR)
         posxml_responses_fn = path.join(self.BASEDIR, 'config', 'posxml_responses.yaml')
         with open(posxml_responses_fn, 'r') as posxml_responses_file:
             self.PXRESPONSES = loadYAML(posxml_responses_file)
+        self.log_fn   = path.join(self.BASEDIR, 'posxml.log')
 
 
     def __enter__(self):
@@ -34,12 +37,20 @@ class PosXML:
         print('Exit PosXML')
 
 
+    def _extractReceipt(self, responseMessage, label):
+        if label in responseMessage:
+            _value = responseMessage[label]
+            print('extracting', label)
+            del responseMessage[label]
+            return _value
+
+
     def post(self, func, data):
-        print('data', data)
+        # print('data', data)
         dict = { 'PosXML': { '@version':'7.2.0', } }
         dict['PosXML'][func] = data
         payload_xml = xmltodict.unparse(dict, pretty=True).encode('utf-8')
-        print('payload_xml', payload_xml)
+        # print('payload_xml', payload_xml)
 
         try:
             http_response = requests.post(self.OPTIONS['url'], data=payload_xml, headers=self.OPTIONS['headers'])
@@ -48,7 +59,6 @@ class PosXML:
             self.bye()
 
         response = xmltodict.parse(http_response.content)['PosXML']
-        print('response', response)
 
         try:
             # find if any key in response matches one of expected response keys
@@ -56,6 +66,28 @@ class PosXML:
         except Exception as e:
             self.feedback({'code': '', 'message': 'Expected responses: "{0}" not present in returned response keys: "{1}".'.format(';'.join(self.PXRESPONSES[func]), ';'.join(response.keys()))}, False)
             self.bye()
+
+        with open(self.log_fn, 'a') as f:
+            f.write('---- {0}\n'.format(datetime.now().isoformat()))
+
+            _payload_txt = dumpsJSON(xmltodict.parse(payload_xml))
+            f.write('Request: \n{0}\n'.format(_payload_txt))
+
+            _MerchReceipt = self._extractReceipt(response[responseKey], 'MerchReceipt')
+            _CustReceipt = self._extractReceipt(response[responseKey], 'CustReceipt')
+
+            _response_txt = dumpsJSON(response)
+            f.write('Response: \n{0}\n'.format(_response_txt))
+
+            if _MerchReceipt:
+                f.write('MerchReceipt: \n{0}\n'.format(_MerchReceipt))
+            if _CustReceipt:
+                f.write('CustReceipt: \n{0}\n'.format(_CustReceipt))
+            f.write('\n')
+
+        log_dict = {'request': dict['PosXML'], 'response': response}
+        # print('PosXML: {0}'.format(dumpsJSON(log_dict, indent=4)))
+
 
         if response[responseKey]['ReturnCode'] != '0':
             self.feedback({
