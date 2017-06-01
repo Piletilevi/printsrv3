8cc44690de4565ca400d5ae8f9902658e6f522ee
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2017-06-01 10:20:22 +0300

    log watcher

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 # This Python file uses the following encoding: utf-8
-import win_unicode_console
-win_unicode_console.enable()
-print('Наличные')
+
+from logWatcher import LogWatcher
+import time
+
+# def callback(source, message):
+#     print('{0}: {1}'.format(source, message))
+def callback(filename, lines):
+    for line in lines:
+        print('{0}: {1}'.format(filename, line))
+
+watcher = LogWatcher('upos', callback)
+
+while 1:
+    watcher.loop(blocking=False)
+    time.sleep(0.1)
